5ea440f295111bcd09c77c15a3b90dc94803f797
i22trmej <150832838+i22trmej@users.noreply.github.com>
2026-02-21 12:40:05 +0000

    codigo FactoriaLlevar

---
+++ practica1/src/factorias/FactoriaLlevar.java
@@ -0,0 +1,22 @@
+public class FactoriaLlevar implements FactoriaAbstracta {
+    private Plato aplicarRecargoLlevar(Plato plato) {
+        plato.asignarPrecio(plato.getPrecio() * 1.02); // Incremento del 2%
+        return plato;
+    }
+
+    @Override
+    public Menu crearMenuSemanal(TipoAcompanamiento acompanamiento) {
+        Menu menu = new MenuSemanal();
+        menu.asignarPlato(aplicarRecargoLlevar(new Plato("Sopa", 5.0, TipoPlato.ENTRANTE, TipoAcompanamiento.NINGUNO)));
+        menu.asignarPlato(aplicarRecargoLlevar(new Plato("Pollo asado", 10.0, TipoPlato.PRINCIPAL, acompanamiento)));
+        // NO incluye postre
+        return menu;
+    }
+
+    @Override
+    public Menu crearMenuTemporada() {
+        Menu menu = new MenuTemporada();
+        menu.asignarPlato(aplicarRecargoLlevar(new Plato("Guiso de invierno", 15.0, TipoPlato.TEMPORADA, TipoAcompanamiento.NINGUNO)));
+        return menu;
+    }
+}
