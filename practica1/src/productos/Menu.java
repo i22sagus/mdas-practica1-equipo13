4605c889c43e5a5e630cdd9ff15c327822276779
i82govij <94856380+i82govij@users.noreply.github.com>
2026-02-21 11:58:26 +0000

    Actualiza Menu.java

---
+++ practica1/src/productos/Menu.java
@@ -0,0 +1,25 @@
+
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class Menu {
+    protected List<Plato> platos;
+
+    public Menu() {
+        this.platos = new ArrayList<>();
+    }
+
+    public abstract double calcularPrecio();
+
+    public List<Plato> obtenerPlatos() {
+        return platos;
+    }
+
+    public void asignarPlato(Plato plato) {
+        if (platos.size() < 3) { // Restricción de 1 a 3 platos según diagrama
+            platos.add(plato);
+        } else {
+            System.out.println("El menú ya tiene el máximo de platos permitidos.");
+        }
+    }
+}
