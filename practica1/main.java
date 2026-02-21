83af915ddc36450e7ef066a9d4fe98e2989604ae
i22trmej <150832838+i22trmej@users.noreply.github.com>
2026-02-21 12:42:26 +0000

    codigo del main

---
+++ practica1/main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("--- PEDIDO EN RESTAURANTE ---");
+        FactoriaAbstracta factoriaLocal = new FactoriaRestaurante();
+        Menu menuLocal = factoriaLocal.crearMenuSemanal(TipoAcompanamiento.PATATAS);
+        imprimirTicket(menuLocal);
+
+        System.out.println("\n--- PEDIDO PARA LLEVAR ---");
+        FactoriaAbstracta factoriaLlevar = new FactoriaLlevar();
+        Menu menuLlevar = factoriaLlevar.crearMenuSemanal(TipoAcompanamiento.ENSALADA);
+        imprimirTicket(menuLlevar);
+    }
+
+    private static void imprimirTicket(Menu menu) {
+        for (Plato p : menu.obtenerPlatos()) {
+            System.out.println("- " + p.toString());
+        }
+        System.out.printf("TOTAL: %.2f€\n", menu.calcularPrecio());
+    }
+}
