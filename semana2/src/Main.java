a7e6c6e42ff1c81bb38a5d9a635732b483a3d206
i82govij <94856380+i82govij@users.noreply.github.com>
2026-03-03 18:45:00 +0000

    feat: añadir pruebas del adaptador en main semana2

--- semana2/src/Main.java
+++ semana2/src/Main.java
@@ -27,5 +27,21 @@ public class Main {
         basico.anadirTexto(f2, "Linea 3 de Fichero 2");
 
         System.out.println("Archivos de prueba creados. Esperando al Adaptador...\n");
+
+        // --- PRUEBAS DEL ADAPTADOR ---
+        MaquetadorAvanzado adaptador = new AdaptadorMaquetador(basico);
+
+        System.out.println("Ejecutando operaciones avanzadas...");
+        
+        adaptador.unirFicheros(f1, f2);
+        System.out.println("- Fichero 2 unido al final del Fichero 1.");
+
+        List<int[]> extracciones = Arrays.asList(new int[]{1, 2}, new int[]{1, 2});
+        adaptador.combinarFicheros(f1, f2, extracciones);
+        System.out.println("- Ficheros combinados en 'combinado.txt'.");
+
+        List<Integer> cortes = Arrays.asList(3);
+        adaptador.separarFichero(f1, cortes);
+        System.out.println("- Fichero 1 separado. Revisa los archivos generados en la carpeta 'semana2/archivos_prueba'.");
     }
 }
